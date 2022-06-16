62ba9fbcede82ade28d6343a133acd1521f8fc62
Tamlinvd <tamlinvandyk05@gmail.com>
2022-06-16 09:16:48 -0700

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,60 @@
 #include "main.h"
 
 /**
- * cap_string - Write a function that capitalizes all words of a string.
- *
- * @entry: This is the input string
- *
- * Return: String capitalized
+ * separator - checks if character is a seperator
+ * @c: character to be checked
+ * Return: if seperator return 1. Otherwise return 0;
  */
-char *cap_string(char *entry)
+
+int separator(char c)
 {
-	int conversion, index, count;
-
-	char chars[] = {' ', ',', ';', '.', '!',
-			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
-	conversion = 32;
-
-	for (index = 0; entry[index] != '\0'; index++)
+	/*Delcaring condition switch*/
+	switch (c)
 	{
-		if (entry[index] >= 'index' && entry[index] <= 'z')
-		{
-			entry[index] =  entry[index] - conversion;
-		}
-		conversion = 0;
-		for (count = 0; chars[count] != '\0'; count++)
-		{
-			if (chars[count] == entry[index])
-			{
-				conversion = 32;
-				break;
-			}
-		}
+	case ' ':
+	case '\t':
+	case '\n':
+	case ',':
+	case ';':
+	case '.':
+	case '!':
+	case '?':
+	case '"':
+	case '(':
+	case ')':
+	case '{':
+	case '}':
+		return (1);
+
+	default:
+		return (0);
 	}
-	return (entry);
+
+}
+/**
+ * cap_string - capitalizes chars after given deliminators
+ * @s: string to uppercase
+ * Return: returns modified string
+ */
+char *cap_string(char *s)
+{
+	int count, upper;
+
+	upper = -32; /*value constant 32*/
+
+	count = 0;
+	/*Start WHILE*/
+	while (s[count] != '\0')
+	{
+		/*letters lowercase*/
+		if (s[count] >= 'a' && s[count] <= 'z')
+		{
+			/*Convert uppercase*/
+			if (s[count] == *s || separator(s[count - 1]))
+
+				s[count] += upper;
+		}
+		count++; /*Add count*/
+	}
+	return (s);
 }
